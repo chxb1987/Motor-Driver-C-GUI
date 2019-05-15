0278f78d563a76abeac62c6b069778026ae83997
JosephRedler <30823461+JosephRedler@users.noreply.github.com>
2019-05-15 09:15:55 +0300

    DebugObj added for debug tab

---
+++ SuperButton MotorController/SuperButton/ViewModels/DebugObjModel.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Windows.Input;
+using SuperButton.Models;
+using SuperButton.Models.DriverBlock;
+using MathNet.Numerics;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Windows.Media;
+using System.Threading.Tasks;
+using System.Linq;
+using SuperButton.CommandsDB;
+using Abt.Controls.SciChart;
+using SuperButton.Views;
+
+namespace SuperButton.ViewModels
+{
+    public class DebugObjModel : ViewModelBase
+    {
+        private string _id;
+        private string _index;
+        private string _getData;
+        private string _setData;
+
+        private bool _intfloat = false;
+
+        public bool IntFloat
+        {
+            get { return _intfloat; }
+            set { _intfloat = value; OnPropertyChanged("IntFloat"); }
+        }
+        public string ID
+        {
+            get { return _id; }
+            set { _id = value; OnPropertyChanged("ID"); }
+        }
+        public string Index
+        {
+            get { return _index; }
+            set { _index = value; OnPropertyChanged("Index"); }
+        }
+        public string GetData
+        {
+            get { return _getData; }
+            set { _getData = value; OnPropertyChanged("GetData"); }
+        }
+        public string SetData
+        {
+            get { return _setData; }
+            set { _setData = value; OnPropertyChanged("SetData"); }
+        }
+        
+        public ActionCommand Get { get { return new ActionCommand(GetCmd); } }
+        public ActionCommand Set { get { return new ActionCommand(SetCmd); } }
+
+        private void GetCmd()
+        {
+            Rs232Interface.GetInstance.SendToParser(new PacketFields
+            {
+                Data2Send = 0,
+                ID = Convert.ToInt16(ID),
+                SubID = Convert.ToInt16(Index),
+                IsSet = false,
+                IsFloat = IntFloat
+            });
+        }
+        private void SetCmd()
+        {
+            Rs232Interface.GetInstance.SendToParser(new PacketFields
+            {
+                Data2Send = Convert.ToInt32(SetData),
+                ID = Convert.ToInt16(ID),
+                SubID = Convert.ToInt16(Index),
+                IsSet = true,
+                IsFloat = IntFloat
+            });
+        }
+    }
+}
---
+++ SuperButton MotorController/SuperButton/ViewModels/DebugViewModel.cs
@@ -0,0 +1,62 @@
+﻿using Abt.Controls.SciChart;
+using SuperButton.Models.DriverBlock;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Collections.ObjectModel;
+using SuperButton.CommandsDB;
+
+namespace SuperButton.ViewModels
+{
+    internal class DebugViewModel : ViewModelBase
+    {
+        #region FIELDS
+        private static readonly object Synlock = new object();
+        private static DebugViewModel _instance;
+        #endregion FIELDS
+        
+        public static DebugViewModel GetInstance
+        {
+            get
+            {
+                lock (Synlock)
+                {
+                    if (_instance != null) return _instance;
+                    _instance = new DebugViewModel();
+                    return _instance;
+                }
+            }
+            set
+            {
+                _instance = value;
+            }
+        }
+        private DebugViewModel()
+        {
+
+        }
+
+        private ObservableCollection<object> _debugList;
+        public ObservableCollection<object> DebugList
+        {
+            get
+            {
+                return Commands.GetInstance.DebugListbySubGroup["DebugList"];
+            }
+            set
+            {
+                _debugList = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
+
+
+
+
+
+
